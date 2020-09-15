9e5798d7f3ce3599f974297649c919bdbc638bb1
Carlosbera7 <carlosbera7@hotmail.com>
2020-09-15 10:48:31 -0300

    Update V003__criaTabelaCidades.sql

--- src/main/resources/db/migration/V003__criaTabelaCidades.sql
+++ src/main/resources/db/migration/V003__criaTabelaCidades.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `estudo`.`CIDADES` (
-  `id_Cidades` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(60) NOT NULL,
   `id_estados` INT NULL,
-  PRIMARY KEY (`id_Cidades`),
+  PRIMARY KEY (`id`),
   INDEX `id_estados_idx` (`id_estados` ASC) VISIBLE,
   CONSTRAINT `id_estados`
     FOREIGN KEY (`id_estados`)
-    REFERENCES `estudo`.`ESTADOS` (`id_estados`)
+    REFERENCES `estudo`.`ESTADOS` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
